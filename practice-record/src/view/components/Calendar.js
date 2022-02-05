60caa974b714af5c6750672546eb35eebe39c175
Durrell Jules <durrelljules@Durrells-Mini.attlocal.net>
2022-02-05 13:25:17 -0500

    added header to calendar, arrow functionality successful

--- practice-record/src/view/components/Calendar.js
+++ practice-record/src/view/components/Calendar.js
@@ -1,14 +1,64 @@
-import React from "react";
-import dateFns from "date-fns";
+import React, { useState } from "react";
+import * as dateFns from "date-fns";
+
+import './Calendar.css'
 
 
 function Calendar () {
-  
+  const [currentMonth, setCurrentMonth] = useState(new Date())
+  const [selectedDate, setSelectedDate] = useState(new Date())
+
+  const renderHeader = () => {
+    const dateFormat = "MMMM yyyy";
+    
+    return (
+    <div className="header row flex-middle">
+      <div className="col col-start">
+        <div className="icon" onClick={() => prevMonth()}>
+          chevron_left
+        </div>
+      </div>
+      <div className="col col-center">
+        <span>
+          {dateFns.format(currentMonth, dateFormat)}
+        </span>
+      </div>
+      <div className="col col-end" onClick={() => nextMonth()}>
+        <div className="icon">chevron_right</div>
+      </div>
+    </div>
+  );
+  };
+
+  const renderDays = () => {
+    
+  };
+
+  const renderCells = () => {
+    
+  };
+
+  const onDateClick = day => {
+
+  };
+
+  const nextMonth = () => {
+    setCurrentMonth(
+      dateFns.addMonths(currentMonth, 1)
+    );
+  };
+
+  const prevMonth = () => {
+    setCurrentMonth(
+      dateFns.subMonths(currentMonth, 1)
+    );
+  };
+
   return(
-    <div>
-      <div>Header</div>
-      <div>Days</div>
-      <div>Cells</div>
+    <div className="calendar">
+      {renderHeader()}
+      {renderDays()}
+      {renderCells()}
     </div>
   );
 };
