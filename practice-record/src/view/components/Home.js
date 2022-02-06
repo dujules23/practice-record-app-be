9ed8240738473f2670db0663105190936103180d
Durrell Jules <durrelljules@Durrells-Mini.attlocal.net>
2022-02-05 20:20:04 -0500

    moved Login component into the Home component

--- practice-record/src/view/components/Home.js
+++ practice-record/src/view/components/Home.js
@@ -1,10 +1,12 @@
-
+import React from "react";
+import Login from './forms/Login'
 
 function Home () {
   return(
     <div>
       <h1>Practice Time!</h1>
       <h3> Get ready for a fun practice session!</h3>
+      <Login/>
     </div>
   );
 }
